d17ae50fd0ec69acad98c5378ecde0ceab07b4fd
sidikone <aboubakone@gmail.com>
2020-11-30 19:01:18 +0100

    ctrv model integration is ok

--- include/evolution_model.py
+++ include/evolution_model.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from utils import cv2DataFrame, ctrv2DataFrame
+
 
 def unit_cv_polar(x_in, y_in, theta_in, speed_in, dt=1):
     """
@@ -27,6 +29,41 @@ def unit_cv_polar(x_in, y_in, theta_in, speed_in, dt=1):
     return x_out, y_out, theta_out, speed_out
 
 
+def unit_ctrv_polar(x_in, y_in, theta_in, speed_in, w_in, dt=1):
+    """
+
+    :param x_in:
+    :param y_in:
+    :param theta_in:
+    :param speed_in:
+    :param w_in:
+    :param dt:
+    :return:
+    """
+
+    if np.abs(w_in) <= 1e-10:
+
+        dx = speed_in * np.cos(theta_in) * dt
+        dy = speed_in * np.sin(theta_in) * dt
+
+    else:
+        ratio = speed_in / w_in
+        dx = ratio * np.sin(dt * w_in) * np.cos(theta_in) + ratio * (
+                np.cos(dt * w_in) * np.sin(theta_in) - np.sin(theta_in)
+        )
+        dy = ratio * (np.cos(theta_in) - np.cos(dt * w_in) * np.cos(theta_in)) + ratio * np.sin(
+            dt * w_in
+        ) * np.sin(theta_in)
+
+    x_out = x_in + dx
+    y_out = y_in + dy
+    theta_out = theta_in + w_in * dt
+    speed_out = deepcopy(speed_in)
+    w_out = deepcopy(w_in)
+
+    return x_out, y_out, theta_out, speed_out, w_out
+
+
 def cv_imu_command_step(state_v_in, a_imu, w_imu, dt_imu):
     """
 
@@ -44,28 +81,27 @@ def cv_imu_command_step(state_v_in, a_imu, w_imu, dt_imu):
     return np.array(state_v_out)
 
 
-# def cv_polar(state_v_in, dt=1):
-#     """
-#
-#     :param state_v_in:
-#     :param dt:
-#     :return:
-#     """
-#
-#     x_in, y_in, theta_in, speed_in = (
-#         state_v_in[0],
-#         state_v_in[1],
-#         state_v_in[2],
-#         state_v_in[3],
-#     )
-#     x_out, y_out, theta_out, speed_out = unit_cv_polar(x_in, y_in, theta_in, speed_in, dt)
-#     state_v_out = np.array([x_out, y_out, theta_out, speed_out])
-#
-#     return state_v_out
+def ctrv_imu_command_step(state_v_in, a_imu, w_imu, dt_imu):
+    """
+
+    :param state_v_in:
+    :param a_imu:
+    :param w_imu:
+    :param dt:
+    :return:
+    """
+    x_t, y_t, theta_t, v_t = state_v_in[0], state_v_in[1], state_v_in[2], state_v_in[3]
+    v_cmd = v_t + a_imu * dt_imu  # speed as a command
+    state_v_out = unit_ctrv_polar(
+        x_in=x_t, y_in=y_t, theta_in=theta_t, speed_in=v_cmd, w_in=w_imu, dt=dt_imu
+    )
+
+    return np.array(state_v_out)
 
 
 def batch_cv_integration(state_init, imu_accel, imu_gyro):
     save_state = []
+    state_v_out = []
     dt = 1. / 100
 
     for count in range(len(imu_accel)):
@@ -89,20 +125,31 @@ def batch_cv_integration(state_init, imu_accel, imu_gyro):
     return cv2DataFrame(state=save_state, ref_data=imu_accel)
 
 
-def cv2DataFrame(state, ref_data):
-    x_ekf, y_ekf = np.array(state)[:, 0], np.array(state)[:, 1]  # pos (m) ekf output
-    yaw_ekf, spd_ekf = (
-        np.array(state)[:, 2],
-        np.array(state)[:, 3],
-    )  # yaw, speed ekf output
-    ekf_index = ref_data.index
+def batch_ctrv_integration(state_init, imu_accel, imu_gyro):
+    save_state = []
+    state_v_out = []
+    dt = 1. / 100
+    # dt_list = compute_timedelta(imu_accel)
 
-    data_frame = pd.DataFrame(
-        {"x": x_ekf, "y": y_ekf, "yaw": yaw_ekf, "speed": spd_ekf},
-        index=ekf_index[: len(x_ekf)],
-    )
+    # for count, dt in enumerate(dt_list):
+    for count in range(len(imu_accel)):
+        if count == 0:
+            state_v = deepcopy(state_init)
+            # save_state.append(state_v)
 
-    return data_frame
+        else:
+            state_v = deepcopy(state_v_out)
+
+        state_v_out = ctrv_imu_command_step(
+            state_v_in=state_v,
+            a_imu=imu_accel[count],
+            w_imu=imu_gyro[count],
+            dt_imu=dt,
+        )
+
+        save_state.append(state_v_out)
+
+    return ctrv2DataFrame(state=save_state, ref_data=imu_accel)
 
 
 def plot_ref_gps(ax):
@@ -127,10 +174,22 @@ def run_batch_cv_kalman(ax):
     return None
 
 
+def run_batch_ctrv_kalman(ax):
+    init = np.array([0, 0, np.deg2rad(183), .0])
+    accel = pd.read_csv('ref_accel.csv')
+    gyro = pd.read_csv('ref_gyro.csv')
+    em_cv = batch_ctrv_integration(state_init=init,
+                                   imu_accel=accel['ref_accel_x (m/s^2)'],
+                                   imu_gyro=np.deg2rad(gyro['ref_gyro_z (deg/s)']))
+    ax.plot(em_cv['x'], em_cv['y'], marker='o', linestyle='--', fillstyle='none', label='CTRV model')
+    return None
+
+
 def main():
     fig, ax = plt.subplots()
     plot_ref_gps(ax=ax)
     run_batch_cv_kalman(ax=ax)
+    run_batch_ctrv_kalman(ax=ax)
     ax.legend()
     plt.show()
     return None
