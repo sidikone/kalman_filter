d422e72d079a725a4d988b94f374ece02d46ade3
sidikone <aboubakone@gmail.com>
2021-01-12 17:40:08 +0100

    timestamp data is ok

--- include/data_loader.py
+++ include/data_loader.py
@@ -32,14 +32,24 @@ class SimuData:
         self.__data_max_len = len(pd.read_csv(local_path))
         self.__timestamp = self.__timestamp_data()
 
-        self.__gps_pos_data = pd.read_csv(path + "/ref_gps.csv")
+        self.__gps_pos_data = pd.read_csv(path + "/ref_pos.csv")
+        self.__add_timestamp_to_data(self.__gps_pos_data, self.__timestamp)
+        self.__gps_spd_data = pd.read_csv(path + "/ref_vel.csv")
+        self.__add_timestamp_to_data(self.__gps_spd_data, self.__timestamp)
         self.__accel_data = pd.read_csv(local_path)
+        self.__add_timestamp_to_data(self.__accel_data, self.__timestamp)
         self.__gyro_data = pd.read_csv(path + "/ref_gyro.csv")
+        self.__add_timestamp_to_data(self.__gyro_data, self.__timestamp)
 
     def __timestamp_data(self):
         time_index = pd.date_range(datetime.datetime.now(), periods=self.__data_max_len,
                                    freq=str(self.__sampling_dt) + 'S', name="timestamp")
         return time_index
+
+    @staticmethod
+    def __add_timestamp_to_data(data, timestamp) -> None:
+        data['Timestamp'] = timestamp.values
+        data.set_index('Timestamp', inplace=True)
 
     def get_imu_data(self, fs=None):
         if fs is None:
