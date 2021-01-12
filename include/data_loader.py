16aeb3cca9c9aa5ba0af590c5c5fd5eb5f6502f3
sidikone <aboubakone@gmail.com>
2021-01-12 16:41:48 +0100

    data getter is an half ok

--- include/data_loader.py
+++ include/data_loader.py
@@ -5,12 +5,12 @@ import datetime
 
 
 class SimuData:
-    def __init__(self, path, sampling_fs, imu_fs, gps_fs):
-
+    def __init__(self, path, sampling_fs):
         self.__path = None
         self.__accel_data = None
         self.__gyro_data = None
-        self.__gps_data = None
+        self.__gps_pos_data = None
+        self.__gps_spd_data = None
         self.__timestamp = None
 
         self.__sampling_dt = None
@@ -24,28 +24,31 @@ class SimuData:
         self.__accel_std = 0
         self.__gyro_std = 0
 
-        self.__initialize(path=path, sampling_fs=sampling_fs, imu_fs=imu_fs, gps_fs=gps_fs)
+        self.__initialize(path=path, sampling_fs=sampling_fs)
 
-    def __initialize(self, path, sampling_fs, imu_fs, gps_fs) -> None:
-
+    def __initialize(self, path, sampling_fs) -> None:
         local_path = path + "/ref_accel.csv"
         self.__sampling_dt = 1. / sampling_fs
-        self.__imu_dt = 1. / imu_fs
-        self.__gps_dt = 1. / gps_fs
         self.__data_max_len = len(pd.read_csv(local_path))
         self.__timestamp = self.__timestamp_data()
+
+        self.__gps_pos_data = pd.read_csv(path + "/ref_gps.csv")
+        self.__accel_data = pd.read_csv(local_path)
+        self.__gyro_data = pd.read_csv(path + "/ref_gyro.csv")
 
     def __timestamp_data(self):
         time_index = pd.date_range(datetime.datetime.now(), periods=self.__data_max_len,
                                    freq=str(self.__sampling_dt) + 'S', name="timestamp")
         return time_index
 
-    def get_ref_imu_data(self, fs=None):
-        dt = 1. / fs
-        data = None
-        if dt is self.__imu_dt:
-            data = pd.read_csv(self.__path)
-        else:
-            data = pd.read_csv(self.__path)
+    def get_imu_data(self, fs=None):
+        if fs is None:
+            self.__imu_dt = self.__sampling_dt
 
-        return data
+        return self.__accel_data, self.__gyro_data
+
+    def get_gps_data(self, fs=None):
+        if fs is None:
+            self.__gps_dt = self.__sampling_dt
+
+        return self.__gps_pos_data, self.__gps_spd_data
