7ed0c14d2b737b9905fd58b1c2d27200f01313aa
sidikone <aboubakone@gmail.com>
2021-01-14 11:42:22 +0100

    data loader is completly final

--- include/data_loader.py
+++ include/data_loader.py
@@ -6,7 +6,7 @@ import datetime
 
 class SimuData:
 
-    def __init__(self, path, sampling_fs):
+    def __init__(self, path):
         self.__path = None
         self.__accel_data = None
         self.__gyro_data = None
@@ -18,27 +18,29 @@ class SimuData:
         self.__sampling_fs = None
         self.__data_max_len = None
 
-        # Add noise to different data
-        self.__gps_pos_std = 0
-        self.__gps_spd_std = 0
-        self.__accel_std = 0
-        self.__gyro_std = 0
+        self.__initialize(path=path)
 
-        self.__initialize(path=path, sampling_fs=sampling_fs)
+    def __initialize(self, path) -> None:
+        local_path = path + "/time.csv"
+        time = pd.read_csv(local_path)
+        dt_median = np.median(time.diff().dropna())
+        dt_median = np.round(dt_median, 5)  # correct precision for sampling frequency until 10,000 Hz
 
-    def __initialize(self, path, sampling_fs) -> None:
-        local_path = path + "/ref_accel.csv"
-        self.__sampling_dt = 1. / sampling_fs
-        self.__sampling_fs = sampling_fs
+        self.__sampling_dt = dt_median
+        self.__sampling_fs = int(1./dt_median)
 
         self.__data_max_len = len(pd.read_csv(local_path))
         self.__timestamp = self.__timestamp_data()
 
         self.__gps_pos_data = pd.read_csv(path + "/ref_pos.csv")
+        self.__gps_pos_data -= self.__gps_pos_data.iloc[0]
+
         self.__add_timestamp_to_data(self.__gps_pos_data, self.__timestamp)
         self.__gps_spd_data = pd.read_csv(path + "/ref_vel.csv")
+        self.__gps_spd_data["ref_vel_norm (m/s)"] = self.__gps_spd_data.apply(np.linalg.norm, axis=1)
         self.__add_timestamp_to_data(self.__gps_spd_data, self.__timestamp)
-        self.__accel_data = pd.read_csv(local_path)
+
+        self.__accel_data = pd.read_csv(path + "/ref_accel.csv")
         self.__add_timestamp_to_data(self.__accel_data, self.__timestamp)
         self.__gyro_data = pd.read_csv(path + "/ref_gyro.csv")
         self.__add_timestamp_to_data(self.__gyro_data, self.__timestamp)
@@ -67,38 +69,39 @@ class SimuData:
 
     @staticmethod
     def __noise_definition(data, std):
-        return data + np.random.normal(scale=np.sqrt(std), size=len(data))
+        return data + np.random.normal(scale=std, size=len(data))
+        # return data + np.random.normal(scale=np.sqrt(std), size=len(data))
 
-    def get_imu_data(self, fs=None):
-        data = ()
+    def get_imu_data(self):
+        return self.__accel_data, self.__gyro_data
+
+    def get_gps_data(self):
+        return self.__gps_pos_data, self.__gps_spd_data
+
+    def set_imu_frequency(self, fs=None) -> None:
         if fs is None:
-            data = (self.__accel_data, self.__gyro_data)
+            pass
 
         else:
             self.__accel_data = self.__resampling_data(data=self.__accel_data, fs_in=self.__sampling_fs, fs_out=fs)
             self.__gyro_data = self.__resampling_data(data=self.__gyro_data, fs_in=self.__sampling_fs, fs_out=fs)
-            data = (self.__accel_data, self.__gyro_data)
-        return data
 
-    def get_gps_data(self, fs=None):
-        data = ()
+    def set_gps_frequency(self, fs=None) -> None:
         if fs is None:
-            data = (self.__accel_data, self.__gyro_data)
+            pass
 
         else:
             self.__gps_pos_data = self.__resampling_data(data=self.__gps_pos_data, fs_in=self.__sampling_fs, fs_out=fs)
             self.__gps_spd_data = self.__resampling_data(data=self.__gps_spd_data, fs_in=self.__sampling_fs, fs_out=fs)
-            data = (self.__accel_data, self.__gyro_data)
-        return data
 
-    def set_accel_data_noise(self, std=0):
-        return self.__add_noise(self.__accel_data, std=std)
+    def set_accel_data_noise(self, std=0) -> None:
+        self.__accel_data = self.__add_noise(self.__accel_data, std=std)
 
-    def set_gyro_data_noise(self, std=0):
-        return self.__add_noise(self.__gyro_data, std=std)
+    def set_gyro_data_noise(self, std=0) -> None:
+        self.__gyro_data = self.__add_noise(self.__gyro_data, std=std)
 
-    def set_gps_pos_data_noise(self, std=0):
-        return self.__add_noise(self.__gps_pos_data, std=std)
+    def set_gps_pos_data_noise(self, std=0) -> None:
+        self.__gps_pos_data = self.__add_noise(self.__gps_pos_data, std=std)
 
-    def set_gps_spd_data_noise(self, std=0):
-        return self.__add_noise(self.__gps_spd_data, std=std)
+    def set_gps_spd_data_noise(self, std=0) -> None:
+        self.__gps_spd_data = self.__add_noise(self.__gps_spd_data, std=std)
